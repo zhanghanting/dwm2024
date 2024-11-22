a38a30e8c16ac0fdfb3303f6d831896723c834b1
zhanghanting <zhanghanting2020@outlook.com>
2024-11-22 14:20:03 +0800

    测试

---
+++ src/test/java/com/atguigu/dwm2024/Dwm2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.dwm2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dwm2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
